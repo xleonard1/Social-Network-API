21bf3a28f795c176ee44408ad25b8f71bc8d386b
xleonard1 <xavier.lnrd@gmail.com>
2022-07-06 14:16:14 -0400

    created user routes

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,55 +1,86 @@
-const { Course, Student } = require('../models');
+const req = require('express/lib/request');
+const res = require('express/lib/response');
+const { Thought, User } = require('../models');
 
 module.exports = {
-  // Get all courses
-  getCourses(req, res) {
-    Course.find()
-      .then((courses) => res.json(courses))
+  // Get all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
-  // Get a course
-  getSingleCourse(req, res) {
-    Course.findOne({ _id: req.params.courseId })
+  // Get a single User by its Id
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
       .select('-__v')
-      .then((course) =>
-        !course
-          ? res.status(404).json({ message: 'No course with that ID' })
-          : res.json(course)
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : res.json({
+            user,
+            email: await email(req.params.userId)
+          })
       )
       .catch((err) => res.status(500).json(err));
   },
-  // Create a course
-  createCourse(req, res) {
-    Course.create(req.body)
-      .then((course) => res.json(course))
+  // update a user by their id
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      {_id: req.params.userId},
+      { $set: req.body }
+    )
+      .then((user) => res.json(user))
       .catch((err) => {
         console.log(err);
         return res.status(500).json(err);
       });
   },
-  // Delete a course
-  deleteCourse(req, res) {
-    Course.findOneAndDelete({ _id: req.params.courseId })
-      .then((course) =>
-        !course
-          ? res.status(404).json({ message: 'No course with that ID' })
-          : Student.deleteMany({ _id: { $in: course.students } })
+  // Delete a user and their associated thoughts
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with that ID' })
+          : Thought.findOneAndUpdate(
+            {users: req.params.userId},
+            {$pull: {users: req.params.userId} },
+            {new: true }
+          )
       )
-      .then(() => res.json({ message: 'Course and students deleted!' }))
+      .then(() => res.json({ message: 'student and thoughts deleted!' }))
       .catch((err) => res.status(500).json(err));
   },
-  // Update a course
-  updateCourse(req, res) {
-    Course.findOneAndUpdate(
-      { _id: req.params.courseId },
-      { $set: req.body },
+  // Add a new friend to the user's friend list.
+  addFriend(req, res) {
+    User.findOneAndUpdate(
+      { _id: req.params.userId },
+      { $addToSet: { friends: req.body } },
       { runValidators: true, new: true }
     )
-      .then((course) =>
-        !course
-          ? res.status(404).json({ message: 'No course with this id!' })
-          : res.json(course)
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No user with this id!' })
+          : res.json(user)
       )
       .catch((err) => res.status(500).json(err));
   },
+
+  // remove a friend from the user
+  removeFriend(req, res) {
+    User.findOneAndUpdate(
+      {_id: req.params.userId },
+      {$pull: {friends: {friendsId: req.params.friendsId }}},
+      { runValidators: true, new: true}
+    )
+    .then((user) => 
+      !user
+        ? res
+            .status(404)
+            .json({ message: 'no user found with that ID'})
+            : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+  },
 };
+
+
--- controllers/studentController.js
+++ controllers/studentController.js
@@ -1,5 +1,5 @@
 const { ObjectId } = require('mongoose').Types;
-const { Student, Course } = require('../models');
+const { User, Thought } = require('../models');
 
 // Aggregate function to get the number of students overall
 const headCount = async () =>
@@ -25,30 +25,24 @@ const grade = async (studentId) =>
 
 module.exports = {
   // Get all students
-  getStudents(req, res) {
-    Student.find()
-      .then(async (students) => {
-        const studentObj = {
-          students,
-          headCount: await headCount(),
-        };
-        return res.json(studentObj);
-      })
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
       .catch((err) => {
         console.log(err);
         return res.status(500).json(err);
       });
   },
   // Get a single student
-  getSingleStudent(req, res) {
-    Student.findOne({ _id: req.params.studentId })
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
       .select('-__v')
-      .then(async (student) =>
-        !student
-          ? res.status(404).json({ message: 'No student with that ID' })
+      .then(async (thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No thought with that ID' })
           : res.json({
-              student,
-              grade: await grade(req.params.studentId),
+              thought,
+              reaction: await grade(req.params.thoughtId),
             })
       )
       .catch((err) => {
@@ -57,7 +51,7 @@ module.exports = {
       });
   },
   // create a new student
-  createStudent(req, res) {
+  createThought(req, res) {
     Student.create(req.body)
       .then((student) => res.json(student))
       .catch((err) => res.status(500).json(err));
